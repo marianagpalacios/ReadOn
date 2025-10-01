2862d6ee63bf4ba15c5de45f808b2600f7fa1b8e
Mariana Gasparotto Palácios <marianagasparottopalacios@gmail.com>
2025-10-01 09:50:10 -0300

    Add files via upload

---
+++ Biblioteca/src/main/java/Controller/LivroControl.java
@@ -0,0 +1,19 @@
+package Controller;
+import Model.Livro;
+
+public class LivroControl {
+    String titulo;
+    String id;
+    String autor;
+    //Avaliacao avaliacoes;
+    String filaReservas;
+    
+    public LivroControl(){
+        //abrir nova conexão com BD 
+    }
+    
+    public void adiciona(Livro livro){
+        //String sql = "insert into Liv4ro(titulo, id, autor, avaliacoes, filaReserva) VALUES(?, ?)";
+        System.out.println("Aluno adicionado com sucesso!");
+    }
+}
