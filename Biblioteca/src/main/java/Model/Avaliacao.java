7eaa847608ec539f5cc9c43d3e864e359f2ae752
Mariana Gasparotto Palácios <marianagasparottopalacios@gmail.com>
2025-10-01 10:24:55 -0300

    Add files via upload

---
+++ Biblioteca/src/main/java/Model/Avaliacao.java
@@ -0,0 +1,33 @@
+package Model;
+
+public class Avaliacao {
+    //String Usuario usuario;
+    int nota;
+    String comentario;
+    
+    /*
+    public Usuario getUsuario(){
+        return usuario;
+    }
+    
+    public void setUsuario(Usuario usuario){
+        this.usuario = usuario;
+    }
+    */
+    
+    public int getNota(){
+        return nota;
+    }
+    
+    public void setNota(int nota){
+        this.nota = nota;
+    }
+    
+    public String getComentario(){
+        return comentario;
+    }
+    
+    public void setComentario(String comentario){
+        this.comentario = comentario;
+    }
+}
