912c0d9aed0588cf5ab08317df7cc488f7fdffd2
Mariana Gasparotto Palácios <marianagasparottopalacios@gmail.com>
2025-10-01 10:56:35 -0300

    Add files via upload

---
+++ Biblioteca/src/main/java/Model/Ebook.java
@@ -0,0 +1,22 @@
+package Model;
+
+public class Ebook {
+    String formato;
+    double tamanhoMb;
+    
+    public String getFormato(){
+        return formato;
+    }
+    
+    public void setFormato(String formato){
+        this.formato = formato;
+    }
+    
+    public double getTamanhoMb(){
+        return tamanhoMb;
+    }
+    
+    public void setTamanhoMb(double tamanhoMb){
+        this.tamanhoMb = tamanhoMb;
+    }
+}
